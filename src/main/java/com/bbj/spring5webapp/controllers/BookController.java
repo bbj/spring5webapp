0945963efec13aec662f3a4093c8fd97168a2a13
Bruno Jullien <bruno.jullien@gmail.com>
2022-09-09 16:15:17 -0700

    21. Configuring Spring MVC Controllers

---
+++ src/main/java/com/bbj/spring5webapp/controllers/BookController.java
@@ -0,0 +1,37 @@
+package com.bbj.spring5webapp.controllers;
+
+import com.bbj.spring5webapp.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+
+        model.addAttribute("books", bookRepository.findAll());
+
+        return "books";
+    }
+}
+
+/*
+    @Controller => Spring managed component => SF will inject an instance of the book repo
+    into the controller, when constructor called
+
+    At runtime:
+    - /books called
+    - controller calls getBooks(Model model)
+    - Spring provide the Model object by injecting it
+    - then getBooks() add a books attrib to model, set value to all bookd
+    - then "books" returned to the view
+
+ */
